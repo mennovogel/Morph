91f3056fcafe94701bda5154c39816f7dd89f335
Menno Vogel <menno@birdly.nl>
2021-08-02 15:28:00 +0200

    Added text to boxes

--- app/src/main/java/nl/birdly/example/Boxes.kt
+++ app/src/main/java/nl/birdly/example/Boxes.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,8 +13,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nl.birdly.morph.Morph
 
 @Preview
@@ -35,7 +39,14 @@ fun Boxes(modifier: Modifier = Modifier) {
                     .clickable {
                         state = BoxesState.END
                     }
-                )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.hello_purple_box),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
             }
             BoxesState.END -> {
                 Box(modifier = Modifier
@@ -44,7 +55,14 @@ fun Boxes(modifier: Modifier = Modifier) {
                     .clickable {
                         state = BoxesState.START
                     }
-                )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.hey_green_box),
+                        fontSize = 12.sp,
+                        color = Color.White,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
             }
         }
     }
